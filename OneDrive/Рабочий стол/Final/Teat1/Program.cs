557a485994965334bc32ed80f6c30503d2cecfc7
Abdulaziz <azizrahmanov@outlook.com>
2024-05-09 20:19:49 +1200

    Second step

--- OneDrive/Рабочий стол/Final/Teat1/Program.cs
+++ OneDrive/Рабочий стол/Final/Teat1/Program.cs
@@ -1,2 +1,26 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
+
+void SecondArrayAddIf (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray (string[] array)
+{
+    for (int i = 0;i<array.Length ;i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+SecondArrayAddIf (array1, array2);
+PrintArray (array2);
